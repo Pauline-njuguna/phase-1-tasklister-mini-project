8f0618d33fc93df0ad0d3ddb721fb91c9e9764ca
pauline-njunguna <paulinenjunguna.w@gmail.com>
2022-12-02 13:59:17 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
+const task= document.getElementById("task");
+task.innerHTML ="New Task"
 document.addEventListener("DOMContentLoaded",(event) => {
   event.preventDefault();
   // your code here
 });
-
-const task= document.getElementById("task");
-task.innerHTML ="New Task"
